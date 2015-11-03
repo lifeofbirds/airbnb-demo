a6341a62bea352cbbf39b16212c83a89c31928f5
jmej <jmejia@anestheticaudio.com>
2015-11-03 14:26:58 -0800

    app scaffolding

---
+++ index.js
@@ -0,0 +1,23 @@
+// Require the twilio and HTTP modules
+var twilio = require('twilio'),
+    http = require('http');
+ 
+// Create an HTTP server, listening on port 1337
+http.createServer(function (req, res) {
+    // Create a TwiML response and a greeting
+    var songUrl = 'http://picosong.com/6Ned'
+    var resp = new twilio.TwimlResponse();
+    resp.play(songUrl);
+ 
+    // The <Gather> verb requires nested TwiML, so we pass in a function
+    // to generate the child nodes of the XML document
+ 
+    //Render the TwiML document using "toString"
+    res.writeHead(200, {
+        'Content-Type':'text/xml'
+    });
+    res.end(resp.toString());
+ 
+}).listen(1337);
+ 
+console.log('Visit http://localhost:1337/ in your browser to see your TwiML document!');
