1058be43ef12d6ca8b50ac9bc46299abb6820221
jmej <jmejia@anestheticaudio.com>
2015-11-13 12:54:43 -0800

    swapped out sound files - added debugging

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Require the twilio and HTTP modules
 var twilio = require('twilio');
 var http = require('http');
+ar fs = require('fs');
+var util = require('util');
+var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
+var log_stdout = process.stdout;
+
+console.log = function(d) { //
+  log_file.write(util.format(d) + '\n');
+  log_stdout.write(util.format(d) + '\n');
+};
 //var audio1Url = require('file-url');
 
 var requestNumber = 0;
@@ -9,13 +18,21 @@ var requestNumber = 0;
 http.createServer(function (req, res) {
     // Create a TwiML response and a greeting
     var audioFiles = [
-    'http://anestheticaudio.com/sounds/doorbell-1.wav',
-    'http://anestheticaudio.com/sounds/doorbell-2.wav',
-    'http://anestheticaudio.com/sounds/doorbell-3.wav',
-    'http://anestheticaudio.com/sounds/doorbell-4.wav',
-    'http://anestheticaudio.com/sounds/doorbell-5.wav',
-    'http://anestheticaudio.com/sounds/doorbell-6.wav',
-    'http://anestheticaudio.com/sounds/doorbell-7.wav'
+    'http://anestheticaudio.com/sounds/1.mp3',
+    'http://anestheticaudio.com/sounds/2.mp3',
+    'http://anestheticaudio.com/sounds/3.mp3',
+    'http://anestheticaudio.com/sounds/4.mp3',
+    'http://anestheticaudio.com/sounds/5.mp3',
+    'http://anestheticaudio.com/sounds/6.mp3',
+    'http://anestheticaudio.com/sounds/7.mp3'
+    'http://anestheticaudio.com/sounds/8.mp3',
+    'http://anestheticaudio.com/sounds/9.mp3',
+    'http://anestheticaudio.com/sounds/10.mp3',
+    'http://anestheticaudio.com/sounds/12.mp3',
+    'http://anestheticaudio.com/sounds/13.mp3',
+    'http://anestheticaudio.com/sounds/14.mp3',
+    'http://anestheticaudio.com/sounds/15.mp3',
+    'http://anestheticaudio.com/sounds/16.mp3'
     ];
 
  // old randomizer fn	
