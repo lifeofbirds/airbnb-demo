d8eeea18a8284857ebddb347457b2b32bfddbd19
jmej <jmejia@anestheticaudio.com>
2015-11-13 14:18:55 -0800

    added html server

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
-// Require the twilio and HTTP modules
-var twilio = require('twilio');
 var http = require('http');
 var fs = require('fs');
 var util = require('util');
+var express = require('express');
+var app = express();
+
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
+
+app.set('view engine', 'jade');
+
 
 console.log = function(d) { //
   log_file.write(util.format(d) + '\n');
   log_stdout.write(util.format(d) + '\n');
 };
-//var audio1Url = require('file-url');
 
 var requestNumber = 0;
- 
-// Create an HTTP server, listening on port 80
-http.createServer(function (req, res) {
-    // Create a TwiML response and a greeting
-    var audioFiles = [
+
+app.get('/', function (req, res) {
+
+  var audioFiles = [
     'http://anestheticaudio.com/sounds/1.mp3',
     'http://anestheticaudio.com/sounds/2.mp3',
     'http://anestheticaudio.com/sounds/3.mp3',
@@ -32,30 +34,22 @@ http.createServer(function (req, res) {
     'http://anestheticaudio.com/sounds/13.mp3',
     'http://anestheticaudio.com/sounds/14.mp3',
     'http://anestheticaudio.com/sounds/15.mp3',
-    'http://anestheticaudio.com/sounds/16.mp3'
+    'http://anestheticaudio.com/sounds/16.mp3',
     ];
 
- // old randomizer fn	
- // 	function randomInt (low, high) {
- // 		return Math.floor(Math.random() * (high - low + 1) + low);
-	// }
+  var index =  requestNumber % audioFiles.length;
+  var audioUrl = audioFiles[index];
 
+  console.log ("Request number "+ requestNumber + " is about to get " + audioUrl);
+  requestNumber++;
 
-	var index =  requestNumber % audioFiles.length;
-    var songUrl = audioFiles[index];
-    var resp = new twilio.TwimlResponse();
-    resp.play(songUrl, {loop: 3000});
- 
- 
-    //Render the TwiML document using "toString"
-    res.writeHead(200, {
-        'Content-Type':'text/xml'
-    });
-    res.end(resp.toString());
+  res.render('index', { audioUrl: audioUrl});
+});
 
-    console.log ("Request number "+ requestNumber + " is listening to " + songUrl);
-    requestNumber++;
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('listening on port 3000');
+});
  
-}).listen(80);
- 
-console.log('server listening on port 80');
